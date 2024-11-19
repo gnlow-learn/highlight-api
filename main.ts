5fe16dfc1f03a8bdeb6e1ee54c354503a21ad2d9
Gnlow <gnlow@protonmail.com>
2024-11-19 20:58:45 +0900

    walk through textnodes

--- main.ts
+++ main.ts
@@ -1,14 +1,27 @@
 const $p = document.querySelector("p")!
 
+function* walk(walker: TreeWalker) {
+    while (walker.nextNode()) {
+        yield walker.currentNode
+    }
+}
+
 const parse = () => {
-    const el = $p.firstChild!
-    const ranges = el.textContent!.matchAll(/[aeiou]+/g)
+    const walker = document.createTreeWalker(
+        $p,
+        NodeFilter.SHOW_TEXT,
+    )
+
+    const ranges = walk(walker).flatMap(el => el.textContent!
+        .matchAll(/[AEIOUaeiou]+/g)
         .map(({ index, [0]: str }) => {
             const range = new Range()
             range.setStart(el, index)
             range.setEnd(el, index + str.length)
             return range
         })
+    )
+
     const highlight = new Highlight(...ranges)
     // @ts-ignore undefined on lib
     CSS.highlights.set("hi1", highlight)
