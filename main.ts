e99b6cc9c3e395d8f3bdc37d535639817c89d374
Gnlow <gnlow@protonmail.com>
2024-11-19 20:21:12 +0900

    it works

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-console.log("hi")
+const $p = document.querySelector("p")!
+
+const parse = () => {
+    const el = $p.firstChild!
+    const ranges = el.textContent!.matchAll(/[aeiou]+/g)
+        .map(({ index, [0]: str }) => {
+            const range = new Range()
+            range.setStart(el, index)
+            range.setEnd(el, index + str.length)
+            return range
+        })
+    const highlight = new Highlight(...ranges)
+    // @ts-ignore undefined on lib
+    CSS.highlights.set("hi1", highlight)
+}
+
+parse()
+
+document.styleSheets[0].insertRule(`
+    ::highlight(hi1) {
+        color: #a55;
+    }
+`)
