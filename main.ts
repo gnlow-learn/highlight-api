27d200c27979fd7c08e11b32fec5b62f181572e6
Gnlow <gnlow@protonmail.com>
2024-11-19 23:24:44 +0900

    diff

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+// deno-lint-ignore no-explicit-any
+declare const CSS: { highlights: any }
+
 const $p = document.querySelector("p")!
 
 function* walk(walker: TreeWalker) {
@@ -23,7 +26,6 @@ const parse = () => {
     )
 
     const highlight = new Highlight(...ranges)
-    // @ts-ignore undefined on lib
     CSS.highlights.set("hi1", highlight)
 }
 
@@ -31,8 +33,36 @@ parse()
 
 $p.addEventListener("input", parse)
 
+let prevRange: Range | null = null
+
+$p.addEventListener("input", _e => {
+    const currRange = getSelection()!.getRangeAt(0)
+    if (prevRange?.startContainer == currRange.startContainer) {
+        const node = currRange.startContainer
+        const range = new Range()
+        range.setStart(node, prevRange.startOffset)
+        range.setEnd(node, currRange.endOffset)
+
+        if (CSS.highlights.has("hi2")) {
+            const highlight = CSS.highlights.get("hi2")
+            highlight.add(range)
+        } else {
+            const highlight = new Highlight(range)
+            CSS.highlights.set("hi2", highlight)
+        }
+    }
+})
+document.addEventListener("selectionchange", _e => {
+    prevRange = getSelection()!.getRangeAt(0)
+})
+
 document.styleSheets[0].insertRule(`
     ::highlight(hi1) {
         color: #a55;
+    }
+`)
+document.styleSheets[0].insertRule(`
+    ::highlight(hi2) {
+        background: #dfd;
     }
 `)
